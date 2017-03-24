c705df0fb430edecc70d44a51f243b907cceaa50
GLaDOS-096 <ZhangYf_CPU@163.com>
2017-03-24 23:27:16 +0800

    first commit

---
+++ event-trigger.js
@@ -0,0 +1,19 @@
+// event-router.js
+
+function EventTrigger(){
+    this.addHandler = function addEventHandler(handler){
+        /* This is what a EventTrigger handler looks like
+           {
+               "event": event,
+               "src": binded_object,
+               "handler": handler_function
+           }
+         */
+    }
+    this.launch = function launch(){
+
+    }
+    this.removeHandler = function removeHandler(handler){
+
+    }
+}
